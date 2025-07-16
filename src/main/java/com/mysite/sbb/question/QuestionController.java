965e054d7bbe73296522410b1c281c778afd9bef
pdd1230 <pddlim@gmail.com>
2025-07-16 17:44:02 +0900

    마크다운 추가하기,  검색기능 추가하기

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -32,12 +32,22 @@ public class QuestionController {
 	private final UserService userService;
 
 	// 질문 목록
-	@GetMapping("/list")
-	public String list(Model model, @RequestParam(value = "page", defaultValue = "0") int page) {
-		Page<Question> paging = this.questionService.getList(page);
-		model.addAttribute("paging", paging);
-		return "question_list";
+//	@GetMapping("/list")
+//	public String list(Model model, @RequestParam(value = "page", defaultValue = "0") int page) {
+//		Page<Question> paging = this.questionService.getList(page);
+//		model.addAttribute("paging", paging);
+//		return "question_list";
+//
+//	}
 
+	@GetMapping("/list")
+	public String list(Model model, @RequestParam(value = "page", defaultValue = "0") int page,
+			@RequestParam(value = "kw", defaultValue = "") String kw) {
+		Page<Question> paging = this.questionService.getList(page, kw);
+		model.addAttribute("paging", paging);
+		// 입력한 검색어를 화면에 그대로 유지
+		model.addAttribute("kw", kw);
+		return "question_list";
 	}
 
 	// 상세조회
--- src/main/java/com/mysite/sbb/question/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/question/QuestionRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
@@ -16,6 +17,8 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
 	List<Question> findBySubjectLike(String subject);
 
 	// 질문 전체 목록을, 지정된 페이지 단위로 가져온다
-	Page<Question> findAll(Pageable pageable);
+//	Page<Question> findAll(Pageable pageable);
+	// Db에서 Question Entity를 조회한 결과를 페이징하여 반환함(중요)
+	Page<Question> findAll(Specification<Question> spec, Pageable pageable);
 
 }
